39ed8a3b7c27db0eff229c4fab179d2fe7ec40a3
thanhgola <thanhgola@gmail.com>
2016-07-30 22:12:23 +0700

    lol

--- Hatcher/Hatcher/Hatcher.ino
+++ Hatcher/Hatcher/Hatcher.ino
@@ -5,8 +5,12 @@
 */
 
 
+#include <DallasTemperature.h>
+#include <OneWire.h>
 #include <EEPROM.h>
 #include <LiquidCrystal.h>
+
+#define ONE_WIRE_BUS 10
 
 int const numberOfButton = 3;
 
@@ -16,14 +20,14 @@ struct Button
 	int state = LOW;
 	int counter = 0;
 	bool holding = false;
-	unsigned long holdTime = 0;
+	byte holdTime = 0;
 	int reading;
 } buttonList[numberOfButton];
 
 unsigned long time = 0;
 int debounce_count = 10;
-const unsigned long hold_time_limit = 150;
-const unsigned long hold_time_step = 20;
+const unsigned int hold_time_limit = 150;
+const unsigned int hold_time_step = 20;
 
 int current_status = 0;
 
@@ -31,6 +35,9 @@ int RELAYhot = A0;
 int RELAYcold = A1;
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
 float tempMax = 37.0f;
 float tempMin = 36.0f;
 float tempNow = 38.0f;
@@ -72,41 +79,40 @@ void decrease()
 
 }
 
-void processButton(Button *button, void(*actionPress)(), void(*actionHold)())
+void processButton(Button &button, void(*actionPress)(), void(*actionHold)())
 {
-	if (button->state == HIGH)
+	if (button.state == HIGH)
 	{
-		button->holdTime++;
-		if (button->holdTime > hold_time_limit)
+		button.holdTime++;
+		if (button.holdTime > hold_time_limit)
 		{
-			button->holding = true;
+			button.holding = true;
 		}
 	}
 	//for hold
-	if (button->holding&&button->holdTime > hold_time_limit)
+	if (button.holding&&button.holdTime > hold_time_limit)
 	{
 		if (actionHold != NULL)
 			actionHold();
-		button->holdTime -= hold_time_step;
-		Serial.print(button->pin);
-		Serial.write("in hold\n");
+		button.holdTime -= hold_time_step;
+		//Serial.print(button->pin);
+		//Serial.write("in hold\n");
 	}
 	//for press
-	else if (button->state == LOW &&button->holdTime > 0 && button->holding == false)
+	else if (button.state == LOW &&button.holdTime > 0 && button.holding == false)
 	{
-		Serial.print(button->pin);
-		Serial.write("in press\n");
+		//Serial.print(button->pin);
+		//Serial.write("in press\n");
 		if (actionPress != NULL)
 			actionPress();
-		button->holdTime = 0;
-		button->holding = false;
+		button.holdTime = 0;
+		button.holding = false;
 	}
-	else if (button->state == LOW)
+	else if (button.state == LOW)
 	{
-		button->holdTime = 0;
-		button->holding = false;
+		button.holdTime = 0;
+		button.holding = false;
 	}
-
 }
 
 void getButtonsState()
@@ -195,7 +201,7 @@ void setThermal()
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	Serial.begin(115200);
+	//Serial.begin(115200);
 
 	buttonList[0].pin = 6;
 	buttonList[1].pin = 7;
@@ -203,6 +209,7 @@ void setup() {
 
 	load();
 
+	sensors.begin();
 	lcd.begin(16, 2);
 
 	pinMode(RELAYhot, OUTPUT);
@@ -219,14 +226,33 @@ void setup() {
 	lcd.print("xin chao");
 	delay(2500);
 }
+unsigned int sensor_count = 0;
+unsigned int display_count = 0;
+unsigned int sensor_fee = 300;
+unsigned int display_fee = 50;
+
 // the loop function runs over and over again until power down or reset
 void loop() {
 	getButtonsState();
-	processButton(&buttonList[0], changeStatus, NULL);
-	processButton(&buttonList[1], decrease, decrease);
-	processButton(&buttonList[2], increase, increase);
+	processButton(buttonList[0], changeStatus, NULL);
+	getButtonsState();
+	processButton(buttonList[1], decrease, decrease);
+	getButtonsState();
+	processButton(buttonList[2], increase, increase);
+	getButtonsState();
 	updateDisplay(tempMin, tempMax, tempNow, current_status);
-	setThermal();
+	getButtonsState();
+
+	if (sensor_count > sensor_fee)
+	{
+		sensors.requestTemperatures();
+		tempNow = sensors.getTempCByIndex(0);
+		setThermal();
+		sensor_count = 0;
+	}
+	getButtonsState();
+
+	sensor_count++;
 }
 
 #pragma endregion
